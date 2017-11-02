7bf1cdfe979f663f2ea793ea5622535a970103fe
ppgiannak <giannakeris@gmail.com>
2017-11-02 17:57:29 +0200

    Update dummy.py

--- src/dummy.py
+++ src/dummy.py
@@ -1,5 +1,15 @@
-import skvideo.io
+import socket
+import sys
+from _thread import *
+import time
+import json
+import urllib.request
+from PIL import Image
+import io
 import os
+import numpy as np
+import requests
+import skvideo.io
 cwd = os.getcwd()
 print(cwd)
 print('Hello World')
