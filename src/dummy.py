2cb1bcea2e2dfaab8c65af95c026b4c2b8c543d6
ppgiannak <giannakeris@gmail.com>
2017-11-03 13:32:59 +0200

    Update dummy.py

--- src/dummy.py
+++ src/dummy.py
@@ -10,9 +10,36 @@ import os
 import numpy as np
 import requests
 import skvideo.io
+
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 9999 # Arbitrary non-privileged port
+
 cwd = os.getcwd()
 print(cwd)
-print('Hello World')
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
-print('Hello World')
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+     
+print('Socket bind complete')
+ 
+#Start listening on socket
+s.listen(10)
+print('Socket now listening') 
+
+while 1:
+    #wait to accept a connection - blocking call
+    print('Waiting for a new connection...')
+    conn, addr = s.accept()
+    print('Connected with ' + addr[0] + ':' + str(addr[1]))
+     
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+ 
+s.close()
